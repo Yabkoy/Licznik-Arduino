117ebd908e9bf5b2c7be604dd63db20654f1083f
Yabkoy <yabuszko05@gmail.com>
2021-05-15 15:23:33 +0200

    multiply to files

---
+++ Licznik/ClockSetDateTimeTest.ino
@@ -0,0 +1,131 @@
+void testMode(int forSwitch)
+{
+    switch(forSwitch)
+    {
+      case 1:
+      {
+        clock.setDateTime(2020, 9, 16, 6, 59, 58); //1
+        break;
+      }
+      case 2:
+      {
+        clock.setDateTime(2020, 9, 16, 7, 59, 57); //2
+        break;
+      }
+      case 3:
+      {
+        clock.setDateTime(2020, 9, 16, 8, 44, 57); //3
+        break;
+      }
+      case 4:
+      {
+        clock.setDateTime(2020, 9, 16, 8, 49, 57); //4
+        break;
+      }
+      case 5:
+      {
+        clock.setDateTime(2020, 9, 16, 8, 59, 57); //5
+        break;
+      }
+      case 6:
+      {
+          clock.setDateTime(2020, 9, 16, 9, 34, 57); //6
+          break;
+      }
+      case 7:
+      {
+        clock.setDateTime(2020, 9, 16, 9, 39, 57); //7
+        break;
+      }
+      case 8:
+      {
+        clock.setDateTime(2020, 9, 16, 9, 59, 57); //8
+        break;
+      }
+      case 9:
+      {
+        clock.setDateTime(2020, 9, 16, 10, 24, 57); //9
+        break;
+      }
+      case 10:
+      {
+        clock.setDateTime(2020, 9, 16, 10, 44, 57); //10
+        break;
+      }
+      case 11:
+      {
+        clock.setDateTime(2020, 9, 16, 10, 59, 57); //11
+        break;
+      }
+      case 12:
+      {
+        clock.setDateTime(2020, 9, 16, 11, 29, 57); //12
+        break;
+      }
+      case 13:
+      {
+        clock.setDateTime(2020, 9, 16, 11, 39, 57); //13
+        break;
+      }
+      case 14:
+      {
+        clock.setDateTime(2020, 9, 16, 11, 59, 57); //14
+        break;
+      }
+      case 15:
+      {
+        clock.setDateTime(2020, 9, 16, 12, 24, 57); //15
+        break;
+      }
+      case 16:
+      {
+        clock.setDateTime(2020, 9, 16, 12, 34, 57); //16
+        break;
+      }
+      case 17:
+      {
+        clock.setDateTime(2020, 9, 16, 12, 59, 57); //17
+        break;
+      }
+      case 18:
+      {
+        clock.setDateTime(2020, 9, 16, 13, 19, 57); //18
+        break;
+      }
+      case 19:
+      {
+        clock.setDateTime(2020, 9, 16, 13, 24, 57); //19
+        break;
+      }
+      case 20:
+      {
+        clock.setDateTime(2020, 9, 16, 13, 59, 57); //20
+        break;
+      }
+      case 21:
+      {
+        clock.setDateTime(2020, 9, 16, 14, 9, 57); //21
+        break;
+      }
+      case 22:
+      {
+        clock.setDateTime(2020, 9, 16, 14, 14, 57); //22
+        break;
+      }
+      case 23:
+      {
+        clock.setDateTime(2020, 9, 16, 14, 59, 57); //23
+        break;
+      }
+      case 24:
+      {
+        clock.setDateTime(2020, 9, 16, 15, 4, 57); //24
+        break;
+      }
+      case 25:
+      {
+        clock.setDateTime(2020, 9, 16, 15, 49, 57); //25
+        break;
+      }
+    }
+}
